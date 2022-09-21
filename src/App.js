53ec3e1b34f826ad5c0495c978f6dd9be851b05a
ken7602jp <k_mouri@dynaxt.co.jp>
2022-09-21 15:01:56 +0900

    auth 4th commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,50 @@
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
+// AmplifySignInをimport
+import { AmplifyAuthenticator, AmplifySignIn } from "@aws-amplify/ui-react";
 
+import Amplify from "aws-amplify";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+// AmplifyAuthenticator内にAmplifySignInを追加し
+// 以下参考に文言を渡していく
 function App() {
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div className='App'>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign Out</button>
-        </div>
-      )}
-    </Authenticator>
+    <AmplifyAuthenticator>
+      <AmplifySignIn
+        slot="sign-in"
+        headerText="サインイン画面"
+        submitButtonText="サインイン"
+        formFields={[
+          {
+            type: "username",
+            label: "サインインID *",
+            placeholder: "ユーザ名を入力",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "パスワード *",
+            placeholder: "パスワードを入力",
+            required: true,
+          },
+        ]}
+      />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+            Learn React
+          </a>
+        </header>
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
